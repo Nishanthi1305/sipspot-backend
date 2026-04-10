6bbf114eaf566dce990f6d228eb11942e95fca28
Nishanthi <nishanthi9342@gmail.com>
2026-04-10 11:11:01 +0530

    fixed role auto insertion

--- src/main/java/com/example/demo/config/DataInitializer.java
+++ src/main/java/com/example/demo/config/DataInitializer.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
